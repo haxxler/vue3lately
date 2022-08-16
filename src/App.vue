08a352eaf3e9b6909c1716bc2f2902cd2716c434
haxxler <haxxkim@gmail.com>
2022-08-16 17:13:46 +0900

    template sytax is completed!!

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <h1>{{ count }}</h1>
+  <!-- <h1 
+  v-once
+  @click='add'>
+    {{ msg }}
+  </h1>
+  <h1 v-html='msg'></h1> -->
+  <h1 
+    :[attr]="'active'"
+    @[event]="add">
+    {{ msg }}
+  </h1>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      count: 2
+      // msg: '<div style="color: red;">Hello!</div>'
+      msg: 'active',
+      attr: 'class',
+      event: 'click'
     }
-  },
-  beforeCreate() {  // 활용도 떨어짐
-    console.log('Before Create!', this.count);
-  },
-  created() {
-    console.log('Created!', this.count);
-  },
-  beforeMount(){ // 활용도 떨어짐
-    console.log('Before Mount!');
-    console.log(document.querySelector('h1'));
-  },
-  mounted() { // 화면에 그려져 있는 내용을 조회하려면, mounted() 라이프 사이클 훅에서
-    console.log('Mounted!');
-    console.log(document.querySelector('h1'));
-   },
-
+  }, 
+  methods: {
+    add() {
+      this.msg += '!'
+    }
+  }
 }
 </script>
+<style scoped>
+  .active {
+    color: rosybrown;
+    font-size: 100px;
+  }
+</style>
