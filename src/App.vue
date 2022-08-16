f6bd24c17d6855dd60a87cef731c1707371448f4
haxxler <haxxkim@gmail.com>
2022-08-16 19:50:18 +0900

    getter and setter test is completed!

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <template>
+  <button @click="add">
+    ADD
+  </button>
   <h1> {{ reversedMessage }}</h1>
   <h1> {{ reversedMessage }}</h1>
   <h1> {{ reversedMessage }}</h1>
@@ -11,14 +14,30 @@
 export default {
   data() {
     return {
+      // Getter, Setter
       msg: 'Hello Computed!!'
     }
   },
   computed: {  // computed는 계산된 결과, 데이터
-    reversedMessage() {
-      return this.msg.split('').reverse().join('')
+    // Getter
+    // reversedMessage() {
+    //   return this.msg.split('').reverse().join('')
+    // }
+    reversedMessage: {
+      get() {
+        return this.msg.split('').reverse().join('')
+      },
+      set(val) {  // reversedMessage의 값을 할당할 때마다 동작
+        this.msg = val;
+        console.log(val);
+      }
     }
   },
+  methods: {
+    add() {
+      this.reversedMessage += '!?'
+    }
+  }
   // methods: {
   //   reverseMessage() {
   //     return this.msg.split('').reverse().join('')
