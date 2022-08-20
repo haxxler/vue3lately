9537fcb26e5e5062371f3fce142c17441768cb50
haxxler <haxxkim@gmail.com>
2022-08-21 00:05:30 +0900

    basic of component is completed!!

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,31 @@
 <template>
-  <button @click="add">
-    ADD
-  </button>
-  <h1> {{ reversedMessage }}</h1>
-  <h1> {{ reversedMessage }}</h1>
-  <h1> {{ reversedMessage }}</h1>
-  <h1> {{ reversedMessage }}</h1>
-  <h1> {{ reversedMessage }}</h1>
-</template> 
+  <!-- <MyBtn text="Banana"/>
+  <MyBtn :color="color" text="Banana"/>
+  <MyBtn 
+    large
+    color="royalblue"
+    text="Banana"/>
+  <MyBtn text="Banana"/> -->
+  <MyBtn>Banana</MyBtn>
+  <MyBtn :color="color">
+  <span style="color: red;">Apple</span>
+  </MyBtn>
+  <MyBtn color="royalblue" large>Cherry</MyBtn>
+  <MyBtn>Orange</MyBtn>
+</template>
 
 <script>
+import MyBtn from '~/components/MyBtn'
 
-export default {
+export default{
+  components: {
+    MyBtn
+  },
   data() {
     return {
-      // Getter, Setter
-      msg: 'Hello Computed!!'
-    }
-  },
-  computed: {  // computed는 계산된 결과, 데이터
-    // Getter
-    // reversedMessage() {
-    //   return this.msg.split('').reverse().join('')
-    // }
-    reversedMessage: {
-      get() {
-        return this.msg.split('').reverse().join('')
-      },
-      set(val) {  // reversedMessage의 값을 할당할 때마다 동작
-        this.msg = val;
-        console.log(val);
-      }
-    }
-  },
-  methods: {
-    add() {
-      this.reversedMessage += '!?'
+      color: '#000'
     }
   }
-  // methods: {
-  //   reverseMessage() {
-  //     return this.msg.split('').reverse().join('')
-  //   }
-  // }
 }
 
 </script>
